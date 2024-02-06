a893b0e883e023c696f9f4938936ff4bf4a51030
MrXdrocks <88088538+TEAM2SUPREMACY@users.noreply.github.com>
2024-02-06 12:09:49 +0700

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,68 @@
+// console.log('hello world');
+
+// var name = "akupertama";
+// console.log(name)
+
+// let panggil = "akuanjay"
+// console.log(panggil)
+
+// const bapak = "bapakkugarena"
+
+// name = "akukedua"
+// console.log(name)
+
+// panggil = "akuhebat"
+// console.log(panggil)
+
+// let firstNumber = 5;
+// let secondNumber = 4;
+// let thirdNumber = 8;
+
+// console.log( firstNumber * secondNumber)
+
+// console.log( firstNumber + secondNumber)
+
+// console.log( firstNumber / thirdNumber)
+
+// console.log( firstNumber % secondNumber)
+
+// console.log( secondNumber>thirdNumber && thirdNumber<secondNumber)
+
+// console.log( secondNumber>firstNumber && thirdNumber>firstNumber)
+
+// let nilai = ("50")
+
+// if(nilai > 90){
+//     console.log("Grade A");
+// } else if (nilai > 75) {
+//     console.log("Grade B");
+// } else if (nilai < 75){
+//     console.log("Grade C");
+// } else {
+//     console.log("Belum Mengerjakan")
+// }
+
+// let angka1
+// let angka2
+// let angka3
+// let angka4
+
+//  if (! (angka1 > angka2 || angka3 > angka4)){
+//     console.log("benar");
+//  } else {
+//     console.log("salah")
+//  }
+
+
+//  for(let i = 100; i > 5; i--){
+//     document.write("<p>Perulangan ke-" + i + "</p>")
+//  }
+
+
+// let num = 1;
+
+// if(num < 10){
+//     for(let num = 1; num <= 10; num++){
+//         document.write("<p>Perulangan ke-" + num + "</p>")
+//     }
+// }
